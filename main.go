e21b078598d640c9444b07be988fa27d4dc373e6
Zhen Dong <devmen@163.com>
2021-07-06 14:47:19 +0800

    modify

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	ding          = kingpin.Flag("ding", "钉钉消息通知接口地址").Short('d').String()
-	dingKey       = kingpin.Flag("dingKey", "钉钉消息通知接口地址").Short('k').Default("自如").String()
+	dingKey       = kingpin.Flag("dingKey", "钉钉消息通知授权KEY（白名单）").Short('k').Default("自如").String()
 	taskInterval  = kingpin.Flag("taskInterval", "任务周期间隔时长（最少5分钟），单位：秒").Short('p').Default("300").Int()
 	TestForNotice = kingpin.Flag("TestForNotice", "[测试] 每次调度推送t个房源通知").Default("0").Short('t').Int()
 	url           = kingpin.Arg("url", "自如网页版请求链接").String()
